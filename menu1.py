da25adc055c062bbb2185f640898d567ff3cd0b7
sanddragon2004 <sanddragon2009@yahoo.com>
2015-09-09 12:05:05 -0700

    Create menu1.py
    This file is a menu shell, use it to create an if else menu.

---
+++ menu1.py
@@ -0,0 +1,23 @@
+print "Enter a selection here. This is where you choose what function you want"
+selection = raw_input("what is your selection?")
+
+# this is an if statement file 
+
+#create a menu that will look at 3 different options and 2 different sub options. 
+
+
+if selection is == "1":
+  print "do you want to do this function"
+#do stuff here!
+
+
+
+elif selection is == "2":
+  print "do you want to do this function instead?"
+#do stuff here!  
+  
+  
+elif selection is == "3":
+  print "do you want to do this function optionally?"
+  
+#do stuff here!   
